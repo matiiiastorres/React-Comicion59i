7b002637287bbbcc24daacbf80f27551cd380f49
MATIAS TORRES <matiiiastorres@gmail.com>
2023-09-13 21:33:14 -0300

    subi los componentes nuevos y la instalacion de boostrap pero nu funciona x la imortacion de stylos en main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Titulo from './Titulo/Titulo';
 import Imagen from './Imagen/Imagen';
+import CardExplample from './Card/CardExplample';
 
 function App() {
   return (
     <>
       <Titulo />
       <Imagen />
+      <CardExplample />
     </>
   );
 }
--- src/Titulo/Titulo.jsx
+++ src/Titulo/Titulo.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Titulo = () => {
   return <div>Proyecto React Comicion 59i</div>;
 };
