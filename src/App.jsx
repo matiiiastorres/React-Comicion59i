8ca7de79caba8f058f56f3073f0392bcad00fbfe
MATIAS TORRES <matiiiastorres@gmail.com>
2023-09-18 19:35:35 -0300

    agregre bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Titulo from './Titulo/Titulo';
 import Imagen from './Imagen/Imagen';
 import CardExplample from './Card/CardExplample';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
