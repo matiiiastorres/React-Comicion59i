fb048e758b59794295940dd9cc7d31b42fac90a3
MattVizcarra <matias.vizcarra.mv@gmail.com>
2023-09-18 20:27:44 -0300

    cree el Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css';
 import Titulo from './Titulo/Titulo';
 import Imagen from './Imagen/Imagen';
 import CardExplample from './Card/CardExplample';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Titulo />
       <Imagen />
       <CardExplample />
