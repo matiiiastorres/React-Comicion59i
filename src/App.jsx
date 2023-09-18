6600500814cab91d1b0762e832624936fe786e6d
lautaro cuello <lautarocuello630@gmail.com>
2023-09-18 20:54:51 -0300

    se resolvieron errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
   import './App.css';
 import Titulo from './Titulo/Titulo';
-import Imagen from './Imagen/Imagen';
-import CardExplample from './Card/CardExplample';
 import UncontrolledExample from './Carrousel/Carrousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,8 +7,6 @@ function App() {
   return (
     <>
       <Titulo />
-      <Imagen />
-      <CardExplample /> 
       <UncontrolledExample/>
     </>
   );
